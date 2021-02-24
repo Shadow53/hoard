71111f1e168741f660a0c90376ea741b6cdfd03d
Michael Bryant <shadow53@shadow53.com>
2021-02-24 13:52:46 -0800

    test(games,config): add unit tests where applicable
    fixes #3

--- src/config.rs
+++ src/config.rs
@@ -6,10 +6,156 @@ use serde::{
     Deserialize,
 };
 use std::{
-    ops::{BitOr, BitOrAssign},
+    ops::BitOr,
     path::PathBuf,
 };
 use structopt::StructOpt;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /// Contains tests relating to the implementation of Default
+    mod defaults {
+        use super::*;
+
+        #[test]
+        fn test_config_default_uses_default_folders() {
+            let config = Config::default();
+            let empty = Config::empty();
+
+            assert_eq!(Some(empty.get_config_path()), config.config);
+            assert_eq!(Some(empty.get_root_path()), config.root);
+            assert_eq!(Some(empty.get_games_path()), config.games_path);
+        }
+
+        #[test]
+        fn test_config_default_log_level_is_info() {
+            let config = Config::default();
+
+            assert_eq!(Some(Level::Info), config.log_level);
+        }
+
+        #[test]
+        fn test_config_default_command_is_none() {
+            let config = Config::default();
+            assert_eq!(None, config.command);
+        }
+    }
+
+    /// Contains tests relating to the logic of finding configured directories
+    mod folders {
+        use super::*;
+
+        #[test]
+        fn test_get_config_file_uses_override() {
+            let mut config = Config::empty();
+            let expected = PathBuf::from("/testing/override");
+            config.config = Some(expected.clone());
+            assert_eq!(expected, config.get_config_path(), "configuration file path should use provided value, if exists");
+        }
+
+        #[test]
+        fn test_get_config_file_defaults_to_system_dir() {
+            let dirs = get_dirs();
+            let expected = dirs.config_dir().join("config.toml");
+            let empty = Config::empty();
+
+            assert_eq!(None, empty.config, "config should be None for test to be valid");
+            assert_eq!(expected, empty.get_config_path(), "configuration file should default to config.toml in project config path");
+        }
+
+        #[test]
+        fn test_get_root_dir_uses_override() {
+            let mut config = Config::empty();
+            let expected = PathBuf::from("/testing/override");
+            config.root = Some(expected.clone());
+            assert_eq!(expected, config.get_root_path(), "saves backup path should use provided value, if exists");
+        }
+
+        #[test]
+        fn test_get_root_dir_defaults_to_system_dir() {
+            let dirs = get_dirs();
+            let expected = dirs.data_dir().join("saves");
+            let empty = Config::empty();
+
+            assert_eq!(None, empty.config, "root should be None for test to be valid");
+            assert_eq!(expected, empty.get_root_path(), "saves backup path should default to `saves/` in project data path");
+        }
+
+        #[test]
+        fn test_get_games_file_uses_override() {
+            let mut config = Config::empty();
+            let expected = PathBuf::from("/testing/override");
+            config.games_path = Some(expected.clone());
+            assert_eq!(expected, config.get_games_path(), "games file path should use provided value, if exists");
+        }
+
+        #[test]
+        fn test_get_games_file_defaults_to_config_sibling() {
+            let mut empty = Config::empty();
+            empty.config = Some(PathBuf::from("/path/to/config.toml"));
+            let expected = PathBuf::from("/path/to/games.toml");
+
+            assert_eq!(None, empty.games_path, "games_path must be None for test to be valid");
+            assert_eq!(expected, empty.get_games_path(), "games file should default to games.toml next to config file");
+        }
+
+        #[test]
+        fn test_get_games_file_uses_config_dir_if_no_parent() {
+            // If the config file is set to a path with no parent (i.e. `/` on Linux),
+            // this avoids crashing by providing a super default.
+            let dirs = get_dirs();
+            let mut empty = Config::empty();
+            empty.config = Some(PathBuf::from("/"));
+            let expected = dirs.config_dir().join("games.toml");
+
+            assert_eq!(None, empty.games_path, "games_path must be None for test to be valid");
+            assert_eq!(expected, empty.get_games_path(), "games file should default to games.toml in project config dir when config file has no parent");
+        }
+    }
+
+    #[test]
+    fn test_config_empty_is_all_none() {
+        let expected = Config {
+            root: None,
+            config: None,
+            games_path: None,
+            log_level: None,
+            command: None,
+        };
+
+        let empty = Config::empty();
+
+        assert_eq!(expected, empty);
+    }
+
+    #[test]
+    fn test_config_bitor_prefers_left() {
+        let none = Config::empty();
+
+        let some_one = Config {
+            root: Some(PathBuf::from("/some/one/root")),
+            config: Some(PathBuf::from("/some/one/config")),
+            games_path: Some(PathBuf::from("/some/one/games")),
+            log_level: Some(Level::Warn),
+            command: Some(Command::Help),
+        };
+
+        let some_two = Config {
+            root: Some(PathBuf::from("/some/two/root")),
+            config: Some(PathBuf::from("/some/two/config")),
+            games_path: Some(PathBuf::from("/some/two/games")),
+            log_level: Some(Level::Error),
+            command: Some(Command::Backup),
+        };
+
+        assert_eq!(none.clone() | some_one.clone(), some_one, "All empty values on left should be replaced by ones on right");
+        assert_eq!(none.clone() | some_two.clone(), some_two, "All empty values on left should be replaced by ones on right");
+        assert_eq!(some_one.clone() | some_two.clone(), some_one, "Non-empty values on left should be preferred");
+        assert_eq!(some_two.clone() | some_one.clone(), some_two, "Non-empty values on left should be preferred");
+    }
+}
 
 pub const CONFIG_FILE_NAME: &str = "config.toml";
 pub const GAMES_LIST_NAME: &str = "games.toml";
@@ -56,7 +202,7 @@ fn default_level() -> Option<Level> {
     Some(Level::Info)
 }
 
-#[derive(Clone, Deserialize, StructOpt)]
+#[derive(Clone, Debug, PartialEq, Deserialize, StructOpt)]
 pub struct Config {
     #[structopt(short, long)]
     root: Option<PathBuf>,
@@ -73,7 +219,31 @@ pub struct Config {
     pub command: Option<Command>,
 }
 
+impl Default for Config {
+    fn default() -> Self {
+        let empty = Config::empty();
+
+        Config {
+            root: Some(empty.get_root_path()),
+            config: Some(empty.get_config_path()),
+            games_path: Some(empty.get_games_path()),
+            log_level: Some(Level::Info),
+            command: None,
+        }
+    }
+}
+
 impl Config {
+    pub fn empty() -> Self {
+        Config {
+            root: None,
+            config: None,
+            games_path: None,
+            log_level: None,
+            command: None,
+        }
+    }
+
     pub fn get_config_path(&self) -> PathBuf {
         self.config
             .clone()
@@ -116,13 +286,7 @@ impl BitOr for Config {
     }
 }
 
-impl BitOrAssign for Config {
-    fn bitor_assign(&mut self, rhs: Self) {
-        *self = self.clone() | rhs
-    }
-}
-
-#[derive(Clone, StructOpt)]
+#[derive(Clone, PartialEq, Debug, StructOpt)]
 pub struct AddGame {
     pub game: String,
     pub ty: GameType,
@@ -131,13 +295,13 @@ pub struct AddGame {
     pub force: bool,
 }
 
-#[derive(Clone, StructOpt)]
+#[derive(Clone, PartialEq, Debug, StructOpt)]
 pub struct RemoveGame {
     pub game: String,
     pub ty: Option<GameType>,
 }
 
-#[derive(Clone, StructOpt)]
+#[derive(Clone, PartialEq, Debug, StructOpt)]
 pub enum Command {
     Help,
     Backup,
--- src/games.rs
+++ src/games.rs
@@ -4,7 +4,36 @@ use std::fmt;
 use std::path::PathBuf;
 use std::str::FromStr;
 
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Deserialize, Serialize)]
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_game_type_display() {
+        assert_eq!("gog", GameType::Gog.to_string());
+        assert_eq!("itch", GameType::Itch.to_string());
+        assert_eq!("native", GameType::Native.to_string());
+        assert_eq!("steam", GameType::Steam.to_string());
+    }
+
+    #[test]
+    fn test_game_type_from_str() {
+        assert_eq!("gog".parse::<GameType>().unwrap(), GameType::Gog);
+        assert_eq!("itch".parse::<GameType>().unwrap(), GameType::Itch);
+        assert_eq!("native".parse::<GameType>().unwrap(), GameType::Native);
+        assert_eq!("steam".parse::<GameType>().unwrap(), GameType::Steam);
+    }
+
+    #[test]
+    fn test_game_type_from_str_is_case_sensitive() {
+        "GOG".parse::<GameType>().unwrap_err();
+        "ITCH".parse::<GameType>().unwrap_err();
+        "NATIVE".parse::<GameType>().unwrap_err();
+        "STEAM".parse::<GameType>().unwrap_err();
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Deserialize, Serialize)]
 #[serde(rename = "lower")]
 pub enum GameType {
     Gog,
