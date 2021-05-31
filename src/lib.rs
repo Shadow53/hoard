84ea2d2a5c3a84b12fba0d9ecbeaa65176f4ab85
Michael Bryant <shadow53@shadow53.com>
2021-05-30 19:45:14 -0700

    chore: enable a bunch more checks

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,49 @@
 #![deny(clippy::style)]
 #![deny(clippy::complexity)]
 #![deny(clippy::perf)]
+#![deny(clippy::pedantic)]
+#![deny(
+    absolute_paths_not_starting_with_crate,
+    anonymous_parameters,
+    bad_style,
+    const_err,
+    dead_code,
+    keyword_idents,
+    improper_ctypes,
+    macro_use_extern_crate,
+    meta_variable_misuse, // May have false positives
+    missing_abi,
+    missing_debug_implementations, // can affect compile time/code size
+    missing_docs,
+    missing_doc_code_examples,
+    no_mangle_generic_items,
+    non_shorthand_field_patterns,
+    noop_method_call,
+    overflowing_literals,
+    path_statements,
+    patterns_in_fns_without_body,
+    pointer_structural_match,
+    private_in_public,
+    semicolon_in_expressions_from_macros,
+    single_use_lifetimes,
+    trivial_casts,
+    trivial_numeric_casts,
+    unaligned_references,
+    unconditional_recursion,
+    unreachable_pub,
+    unsafe_code,
+    unused,
+    unused_allocation,
+    unused_comparisons,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_lifetimes,
+    unused_parens,
+    unused_qualifications,
+    unused_results,
+    variant_size_differences,
+    while_true
+)]
 
 pub use config::builder::Builder;
 
