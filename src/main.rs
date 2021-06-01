d2d849afaf5cfb4f22dc0e8362ceaac7e8015c95
Michael Bryant <shadow53@shadow53.com>
2021-05-31 17:48:40 -0700

    feat: set up logger

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
+use env_logger::Builder;
 use hoard::config::Error;
 use hoard::Config;
+use log::LevelFilter;
 
 fn main() -> Result<(), Error> {
+    // Set up default logging
+    let mut builder = Builder::new();
+    builder.filter_level(if cfg!(debug_assertions) {
+        LevelFilter::Debug
+    } else {
+        LevelFilter::Info
+    });
+    builder.parse_env("HOARD_LOG");
+    builder.init();
+
+    // Get configuration
     let config = Config::load()?;
+
+    // Use configured log level
+    log::set_max_level(config.log_level.to_level_filter());
+
+    // Run command with config
     config.run()
 }
