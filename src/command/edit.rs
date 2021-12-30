4826f67d651da5bda909cb932aa53dd75f18f6d8
Michael Bryant <shadow53@shadow53.com>
2021-12-29 22:13:48 -0800

    style: run cargo fmt

--- src/command/edit.rs
+++ src/command/edit.rs
@@ -1,6 +1,6 @@
 use atty::Stream;
-use thiserror::Error;
 use std::{path::Path, process::ExitStatus};
+use thiserror::Error;
 
 #[derive(Debug, Error)]
 #[allow(variant_size_differences)]
@@ -8,13 +8,14 @@ pub enum Error {
     #[error("failed to start editor: {0}")]
     Start(#[from] open_cmd::Error),
     #[error("editor exited with failure status: {0}")]
-    Exit(ExitStatus)
+    Exit(ExitStatus),
 }
 
 pub(crate) fn edit(path: &Path) -> Result<(), Error> {
     let _span = tracing::trace_span!("edit", ?path).entered();
 
-    let mut cmd = if atty::is(Stream::Stdout) && atty::is(Stream::Stderr) && atty::is(Stream::Stdin) {
+    let mut cmd = if atty::is(Stream::Stdout) && atty::is(Stream::Stderr) && atty::is(Stream::Stdin)
+    {
         open_cmd::open_editor(path.to_owned())?
     } else {
         open_cmd::open(path.to_owned())?
