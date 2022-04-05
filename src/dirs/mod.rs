45d14fcdae949e207849eb50365b796b0cdd308c
Michael Bryant <shadow53@shadow53.com>
2022-04-04 18:41:46 -0700

    refactor!: introduce env vars for overriding config/data dirs

--- src/dirs/mod.rs
+++ src/dirs/mod.rs
@@ -1,5 +1,5 @@
 //! Functions to determine special folders for Hoard to work with on different platforms.
-use std::path::PathBuf;
+use std::path::{PathBuf, Path};
 
 #[cfg(unix)]
 mod unix;
@@ -22,6 +22,10 @@ pub const TLD: &str = "com";
 pub const COMPANY: &str = "shadow53";
 /// The Project Name portion of the application identifier.
 pub const PROJECT: &str = "hoard";
+/// The environment variable that takes precendence over data dir detection.
+pub const DATA_DIR_ENV: &str = "HOARD_DATA_DIR";
+/// The environment variable that takes precendence over config dir detection.
+pub const CONFIG_DIR_ENV: &str = "HOARD_CONFIG_DIR";
 
 #[inline]
 fn path_from_env(var: &str) -> Option<PathBuf> {
@@ -40,6 +44,8 @@ pub fn home_dir() -> PathBuf {
 
 /// Returns Hoard's configuration directory for the current user.
 ///
+/// Returns the contents of `HOARD_CONFIG_DIR`, if set, otherwise:
+///
 /// - Windows: `{appdata}/shadow53/hoard/config` where `{appdata}` is the "known folder"
 ///   `FOLDERID_RoamingAppData` or the value of `%APPDATA%`.
 /// - macOS: `${XDG_CONFIG_HOME}/hoard`, if `XDG_CONFIG_HOME` is set, otherwise
@@ -48,7 +54,7 @@ pub fn home_dir() -> PathBuf {
 #[must_use]
 #[inline]
 pub fn config_dir() -> PathBuf {
-    sys::config_dir()
+    path_from_env(CONFIG_DIR_ENV).unwrap_or_else(sys::config_dir)
 }
 
 /// Returns Hoard's data directory for the current user.
@@ -61,5 +67,19 @@ pub fn config_dir() -> PathBuf {
 #[must_use]
 #[inline]
 pub fn data_dir() -> PathBuf {
-    sys::data_dir()
+    path_from_env(DATA_DIR_ENV).unwrap_or_else(sys::data_dir)
+}
+
+/// Set the environment variable that overrides Hoard's config directory.
+///
+/// See [`CONFIG_DIR_ENV`].
+pub fn set_config_dir(path: &Path) {
+    std::env::set_var(CONFIG_DIR_ENV, path);
+}
+
+/// Set the environment variable that overrides Hoard's data directory.
+///
+/// See [`DATA_DIR_ENV`].
+pub fn set_data_dir(path: &Path) {
+    std::env::set_var(DATA_DIR_ENV, path);
 }
